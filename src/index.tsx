01c9704d938458c074a725e3f402769e4f8d2835
apjavier2 <apjavier27@gmail.com>
2023-03-15 15:08:50 +0800

    created initial components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,25 @@
 import ReactDOM from "react-dom";
+import { useState } from "react";
 
 const App = () => {
-  return <h1>Hello!</h1>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const textHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(event.target.value);
+  };
+
+  const submitHandler = () => {};
+
+  return (
+    <div>
+      <textarea value={input} onChange={textHandler}></textarea>
+      <div>
+        <button onClick={submitHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
