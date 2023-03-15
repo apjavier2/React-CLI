db56200ad022be0850f32349e21319a0ddeb5c4e
apjavier2 <apjavier27@gmail.com>
2023-03-15 15:29:33 +0800

    applied code transpiling using esbuild-wasm

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,43 @@
+import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+
+  const startService = async () => {
+    //initialization: this allows us to access the service in any part of our code
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  //calling start service upon rendering once
+  useEffect(() => {
+    startService();
+  }, []);
 
   const textHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInput(event.target.value);
   };
 
-  const submitHandler = () => {};
+  const submitHandler = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    //transpiling the code
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    //set the transformed code
+    setCode(result.code);
+  };
 
   return (
     <div>
