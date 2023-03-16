dc95be7c7668498b3eab93c0fd81c6dcf1a709f6
apjavier2 <apjavier27@gmail.com>
2023-03-16 09:07:12 +0800

    modified code in unpkg-plugin to resolve and load nested paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -9,12 +9,22 @@ export const unpkgPathPlugin = () => {
         console.log("onResolve", args);
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
-        } else if (args.path === "tiny-test-pkg") {
+        }
+        if (args.path.includes("./") || args.path.includes("../")) {
           return {
-            path: "https://www.unpkg.com/tiny-test-pkg@1.0.0/index.js",
+            //args.path ex: ./utils
+            //2nd argument: https://www.unpkg.com/{resolveDir}/
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
             namespace: "a",
           };
         }
+        return {
+          path: `https://www.unpkg.com/${args.path}`,
+          namespace: "a",
+        };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -24,16 +34,18 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from 'tiny-test-pkg';
+              import message from 'react';
               console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
         return {
           loader: "jsx",
           contents: data,
+          //resolveDir returns the path of the last loaded file
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
       });
     },
