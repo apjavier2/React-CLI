a201cf15b9bf9aebd705df388efd39abc7cef005
apjavier2 <apjavier27@gmail.com>
2023-03-16 09:43:17 +0800

    implement caching on loading packages

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
+import localforage from "localforage";
+
+const fileCache = localforage.createInstance({
+  name: "filecache",
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -40,13 +45,28 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        //check to see if we already fetched this file and it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        //if it is, return immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           //resolveDir returns the path of the last loaded file
           resolveDir: new URL("./", request.responseURL).pathname,
         };
+
+        //store response in cache
+        await fileCache.setItem(args.path, result);
+        return result;
       });
     },
   };
