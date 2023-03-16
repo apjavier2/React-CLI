adbfa748816473955d602b7c2a426d0879f5cdf0
apjavier2 <apjavier27@gmail.com>
2023-03-16 15:07:32 +0800

    connected the codeEditor changes to the inputState

--- src/index.tsx
+++ src/index.tsx
@@ -73,7 +73,10 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor />
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
       <textarea value={input} onChange={textHandler}></textarea>
       <div>
         <button onClick={submitHandler}>Submit</button>
