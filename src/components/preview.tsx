3aff050d243b586cd32f8ee110a781c50d6fe364
apjavier2 <apjavier27@gmail.com>
2023-03-21 10:28:21 +0800

    bug fix in iframe

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -8,25 +8,26 @@ interface PreviewProps {
 //create an html file with eventlistener
 //this also catches sync errors
 const html = `
-  <html>
-    <head></head>
-    <body>
-      <div id="root"></div>
-      <script>
-        window.addEventListener('message', (event) => {
-          try{
-          eval(event.data);
-          }catch(err){
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color:red;"><h4>Runtime Error</h4>'+err+'</div>';
-            console.error(err);
-          }
-        }, false);
-      </script>
-    </body>
-  </html>
-`;
-
+    <html>
+      <head>
+        <style>html { background-color: white; }</style>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 const Preview: React.FC<PreviewProps> = ({ code }) => {
   const iframe = useRef<any>();
   useEffect(() => {
