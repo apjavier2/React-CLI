b35862334972970cde79de6373c432fd6f28d9e3
apjavier2 <apjavier27@gmail.com>
2023-03-21 08:27:51 +0800

    created separate file for bundler

--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,21 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import * as esbuild from "esbuild-wasm";
+
 import ReactDOM from "react-dom";
-import { useState, useEffect, useRef } from "react";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import { useState } from "react";
+
 import CodeEditor from "./components/code-editor";
 import Preview from "./components/preview";
 
-const App = () => {
-  const ref = useRef<any>();
+import bundle from "./bundler";
 
+const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const startService = async () => {
-    //initialization: this allows us to access the service in any part of our code
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-
-  //calling start service upon rendering once
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const textHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInput(event.target.value);
-  };
-
   const submitHandler = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    //send message to child using postMessage
-
-    setCode(result.outputFiles[0].text);
+    //pass raw code to bundler
+    const output = await bundle(input);
+    setCode(output);
   };
 
   return (
