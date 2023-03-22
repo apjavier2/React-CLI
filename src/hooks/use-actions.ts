f82b26573d18483c7e41cfdb76c7acfc990daf7d
apjavier2 <apjavier27@gmail.com>
2023-03-22 14:58:50 +0800

    added UseMemo in use-actions to make sure that it is only created once and will not cause a loop in useEffect dependency

--- src/hooks/use-actions.ts
+++ src/hooks/use-actions.ts
@@ -1,10 +1,14 @@
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../state";
+import { useMemo } from "react";
 
 export const useActions = () => {
   const dispatch = useDispatch();
   //this will return an object containing all the different action creators,
   //the action will automatically be provided to dispatch
-  return bindActionCreators(actionCreators, dispatch);
+
+  return useMemo(() => {
+    return bindActionCreators(actionCreators, dispatch);
+  }, [dispatch]);
 };
