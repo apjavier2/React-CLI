a98c5ba31c87422abfddbb4c21286967d5e5ca02
apjavier2 <apjavier27@gmail.com>
2023-03-22 10:20:28 +0800

    created action bar

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -39,7 +39,7 @@ const reducer = produce(
         delete state.data[action.payload];
 
         //delete in order
-        state.order.filter((id) => id !== action.payload);
+        state.order = state.order.filter((id) => id !== action.payload);
 
         return state;
       case ActionType.MOVE_CELL:
