f754d8727517f3cc333f9c627f59d4f230e496ec
apjavier2 <apjavier27@gmail.com>
2023-03-22 09:20:40 +0800

    create use-action hooks

---
+++ src/hooks/use-actions.ts
@@ -0,0 +1,10 @@
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../state";
+
+export const useActions = () => {
+  const dispatch = useDispatch();
+  //this will return an object containing all the different action creators,
+  //the action will automatically be provided to dispatch
+  return bindActionCreators(actionCreators, dispatch);
+};
