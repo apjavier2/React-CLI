fa82711b413958ce7689112e58191a128135c0ed
apjavier2 <apjavier27@gmail.com>
2023-03-22 11:31:42 +0800

    added css force visibility in add-cell component if there are no remaining cells on the screen

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -21,7 +21,7 @@ const CellList: React.FC = () => {
   return (
     <div>
       {renderedCells}
-      <AddCell nextCellId={null} />
+      <AddCell nextCellId={null} forceVisible={cells.length === 0} />
     </div>
   );
 };
