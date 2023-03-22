a06b0157eaa862af457ea691db74c4e6bc9c0964
apjavier2 <apjavier27@gmail.com>
2023-03-22 16:30:43 +0800

    fix issue with show function showing in other cells' screens

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -25,27 +25,34 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     //get a list of ordered cells
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode = [
-      `
-      import _React from 'react';
-      import _ReactDOM from 'react-dom';
-      const root = document.querySelector('#root');
-      const show = (value) => {
-        if(typeof value === 'object'){
-          if(value.$$typeof && value.props){
-            _ReactDOM.render(value, root);
-          }else{
-            root.innerHTML = JSON.stringify(value);
-          }
+    const showFuncNoop = `var show = () => {}`;
+    const showFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+    const root = document.querySelector('#root');
+    var show = (value) => {
+      if(typeof value === 'object'){
+        if(value.$$typeof && value.props){
+          _ReactDOM.render(value, root);
         }else{
-          root.innerHTML = value;
+          root.innerHTML = JSON.stringify(value);
         }
-      
-    };`,
-    ];
+      }else{
+        root.innerHTML = value;
+      }
+  };`;
+
+    const cumulativeCode = [];
 
     for (let c of orderedCells) {
       if (c.type === "code") {
+        if (c.id === cell.id) {
+          //this will give a working show function
+          cumulativeCode.push(showFunc);
+        } else {
+          //this overrides the showFunctions from other cells, making it not print anything in the screen.
+          cumulativeCode.push(showFuncNoop);
+        }
         cumulativeCode.push(c.content);
       }
       //only the previous cell codes will be added. Stop iteration in the current cell
