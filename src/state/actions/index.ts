72d1ab9f1de8bb052d06b5bc1680e52c2da03e1b
apjavier2 <apjavier27@gmail.com>
2023-03-22 13:34:21 +0800

    added action types and action interfaces for bundling

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -31,8 +31,30 @@ export interface UpdateCellAction {
   };
 }
 
+//cellId of the cell that is currently bundling
+export interface BundleStartAction {
+  type: ActionType.BUNDLE_START;
+  payload: {
+    cellId: string;
+  };
+}
+
+//cellId of the cell that is currently bundling
+export interface BundleCompleteAction {
+  type: ActionType.BUNDLE_COMPLETE;
+  payload: {
+    cellId: string;
+    bundle: {
+      code: string;
+      err: string;
+    };
+  };
+}
+
 export type Action =
   | MoveCellAction
   | DeleteCellAction
   | InsertCellAfterAction
-  | UpdateCellAction;
+  | UpdateCellAction
+  | BundleStartAction
+  | BundleStartAction;
