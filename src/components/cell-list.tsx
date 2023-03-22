a5980b7c5363363b32d5943acbf5e0d60bf24a60
apjavier2 <apjavier27@gmail.com>
2023-03-22 10:57:11 +0800

    added addcell component (w/ action creators)

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,5 @@
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import AddCell from "./add-cell";
 import CellListItem from "./cell-list-item";
 
 const CellList: React.FC = () => {
@@ -10,10 +11,18 @@ const CellList: React.FC = () => {
   });
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem key={cell.id} cell={cell} />
+    </>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
